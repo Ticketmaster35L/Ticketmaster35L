a61f7f69bf663017daba84493c586e59ff83f9ef
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2023-02-21 10:16:21 -0800

    before Rollup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Test from './test.js'
+import express from 'express'
 import reportWebVitals from './reportWebVitals';
+
+const app = express();
+
+app.use('/test', Test);
+  
+app.listen(3000);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
