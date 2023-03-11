b4b8eab71e09be73544023d7066503926b2a9da2
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2023-03-10 16:59:35 -0800

    updated test form to work with backend

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.urlencoded());
+
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+app.post('/test_endpoint', (req, res) => {
+  console.log(req.body)
 });
 
 app.get('/', (req, res) => {
