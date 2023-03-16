77f97c7865fd1c5ca9756bf3d0739954fa81fa85
BrianFellDownTheWell <brianlyang@outlook.com>
2023-03-15 21:21:18 -0700

    Got the antd grid to show up on the screen.

--- src/components/DisplayTicket.jsx
+++ src/components/DisplayTicket.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Grid } from 'antd';
+import { Row, Col } from 'antd';
 
 const DisplayTicket = () => {
-    const {Row, Col } = Grid;
     return (
         <>
         <p>
             TestPrint
         </p>
-        {/* <Row gutter={48}>
+        <Row gutter={[48,48]}>
             <Col span={12}>Col1</Col>
-                <p>col1</p>
             <Col span={12}>Col2</Col>
             <Col span={12}>Col3</Col>
-        </Row> */}
+        </Row>
         </>
     )
 }
