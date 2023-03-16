352b6e41afe70539773850468e7de028c5e92764
Ajzboss <ajano3191@gmail.com>
2023-03-16 05:49:03 -0700

    Updates Info Dynamically in name file

--- src/components/blog/Post.jsx
+++ src/components/blog/Post.jsx
@@ -1,45 +1,64 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import {useState} from 'react'
+import { Form, Input, Row, Col, Card, Table, Button } from 'antd';
+import { Descriptions, Radio } from 'antd';
+
 function Post() {
+  
   let { postSlug } = useParams();
-  const [ticket, setTicket] = useState({})
+  const [ticket, setTicket] = useState([{}])
+
   useEffect(() => {
     // Fetch post using the postSlug
+    console.log("refreshing")
     fetch('/api/ticket/'+postSlug).then((response) => response.json())
     .then((ticket)=> {
-      /*I'll let you figure out how to handle this, but data here is a dictionairy with every id of the ticket
-      as keys and the ticket object as values.Proccess and add to the table as you need!*/
-      // setTicket(ticket)
       console.log(ticket)
       setTicket(ticket)
     })
 
   }, [postSlug]);
+  const handleTicketChange = (index,event) => {
+    let data = ticket;
+    data[index] = event.target.value;
+    setTicket(data);
+    fetch('/api/ticket/'+postSlug,{
+      method: "POST",
+      body: JSON.stringify(ticket),
+    }
+    ).then((response) => response.json()).then((data) => console.log(data))
+    console.log(ticket.name)
+    //alert(event.target.value)
+  }
+  const { TextArea } = Input;
 
+  const input_field = {
+    border : 0,
+    width: '100%',
+    height:"100%"
+  };
   return (
+    
     <div className="home">
       <div class="container">
-        <h1 className="mt-5">This is a Post Title</h1>
-        <h6 className="mb-5">The post slug is,{ticket.name}</h6>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
+        <div className="ticketDescriptor">
+            <div class="container"></div>
+            <Descriptions title="Ticket Info" bordered>
+            <Descriptions.Item label="Ticket Name"><input style={input_field} type="text" defaultValue={ticket.name} onChange={event => handleTicketChange("name",event)}/></Descriptions.Item>
+            <Descriptions.Item label="Ticket Status"><Input value={ticket.status} bordered={false}/></Descriptions.Item>
+            </Descriptions>
+            <Form name="Comments">
+                    <Form.Item label="Insert a comment here" name="commentForm">
+                        <TextArea name="Comment Field" rows={6} cols={100}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+        </div>
       </div>
     </div>
   );
