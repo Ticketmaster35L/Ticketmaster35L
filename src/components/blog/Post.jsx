ac7ab2e79e1e4df26f3568437c0d725e8905f877
Ajzboss <ajano3191@gmail.com>
2023-03-16 20:58:15 -0700

    Updated Tickets

--- src/components/blog/Post.jsx
+++ src/components/blog/Post.jsx
@@ -1,45 +1,66 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import {useState} from 'react'
-import { Form, Input, Row, Col, Card, Table, Button } from 'antd';
+import { Form, Input, Row, Col, Card, Table, Button,Select,DatePicker } from 'antd';
 import { Descriptions, Radio } from 'antd';
+import { NavLink } from "react-router-dom";
+import dayjs from 'dayjs';
+
 
 function Post() {
   
   let { postSlug } = useParams();
   const [ticket_name, setTicket_name] = useState("")
   const [ticket_status, setTicket_status] = useState("")
-  const [ticket_creationDate, setTicket_creationDate] = useState("")
+  const [ticket_dueDate, setTicket_dueDate] = useState("")
+  const [ticket_assignedUser, setTicket_assignedUser] = useState("")
+  const [ticket_assignedDate, setTicket_assignedDate] = useState("")
 
   useEffect(() => {
     // Fetch post using the postSlug
     console.log("refreshing")
     fetch('/api/ticket/'+postSlug).then((response) => response.json())
     .then((ticket)=> {
-      console.log(ticket)
       setTicket_name(ticket.name)
       setTicket_status(ticket.status)
-      setTicket_creationDate(ticket.creationDate)
-      console.log(ticket_name) 
-
+      setTicket_dueDate(dayjs(ticket.dueDate))
+      setTicket_assignedUser(ticket.assignedUser)
+      setTicket_assignedDate(dayjs(ticket.assignedDate))
     })
 
   }, [postSlug]);
   const handleTicketChange = (index,event) => {
+    console.log(index)
+    if(index=="dueDate"){      
+      if(event.$d == "Invalid Date"|| event.$d==null){
+        console.log("INvalid Date")
+        return}}
+    console.log(event)
     let data = {
       //EVERY FIELD MUST BE IN HERE
       name:ticket_name,
       status:ticket_status,
-      assignedUser:"",
-      creationDate:ticket_creationDate,
-      dueDate:"",
+      assignedUser:ticket_assignedUser,
+      duedate:ticket_dueDate,
+      assignedDate:"",
       description:"",
       language:""
     };
-    data[index] = event.target.value;
+    if(index=="status"){
+      data[index] = event;
+    }
+    else if(index="duedate"){
+      data[index]= event.$d
+      console.log(dayjs(data[index]))
+    }
+    else{
+      data[index] = event.target.value;
+
+    }
     //SET EVERY TICKET FIELD'S STATE HERE
     setTicket_name(data.name)
     setTicket_status(data.status)
+    setTicket_dueDate(dayjs(data["duedate"]))
     //console.log(ticket_status)
     fetch('/api/ticket/'+postSlug,{
       method: "POST",
@@ -53,6 +74,7 @@ function Post() {
 
     //alert(event.target.value)
   }
+  
   const { TextArea } = Input;
   return (
     
@@ -62,7 +84,30 @@ function Post() {
             <div class="container"></div>
             <Descriptions title="Ticket Info" bordered>
             <Descriptions.Item label="Ticket Name"><Input name="ticket_name" type="text" value={ticket_name} onChange={event => handleTicketChange("name",event)} bordered={false} /></Descriptions.Item>
-            <Descriptions.Item label="Ticket Status"><Input name="ticket_status" type="text" value={ticket_status} onChange={event => handleTicketChange("status",event)} bordered={false}/></Descriptions.Item>
+            <Descriptions.Item label="Assigned User" span={2}><Input name="Assigned User" type="text" value={ticket_assignedUser} onChange={event => handleTicketChange("assignedUser",event)} bordered={false}/></Descriptions.Item>
+            <Descriptions.Item  label="Ticket Status"span={3} >
+              
+            <Select value={ticket_status} options={[
+        {
+          value: 'Unfinished',
+          label: 'Unfinished',
+        },
+        {
+          value: 'In Progress',
+          label: 'In Progress',
+        },
+        {
+          value: 'Finished',
+          label: 'Finished',
+        },
+      ]}
+      style={{width: '100%'}} name="ticket_status" onChange={event => handleTicketChange("status",event)} bordered={false} showArrow={false} />
+              
+              </Descriptions.Item>
+              
+               <Descriptions.Item label="Due Date"span={2}><DatePicker value={ticket_dueDate} onChange={event => handleTicketChange("dueDate",event)} bordered={false}/></Descriptions.Item> 
+              <Descriptions.Item label="Assigned Date"span={1}><DatePicker value={ticket_assignedDate} disabled/></Descriptions.Item>
+
             </Descriptions>
             <Form name="Comments">
                     <Form.Item label="Insert a comment here" name="commentForm">
@@ -74,6 +119,9 @@ function Post() {
                         </Button>
                     </Form.Item>
                 </Form>
+                <NavLink className="nav-link" to={-1}>
+                Go Back
+            </NavLink>
         </div>
       </div>
     </div>
