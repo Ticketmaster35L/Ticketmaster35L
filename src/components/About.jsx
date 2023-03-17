a0cd9075434bb8c95fb45a3bc7d779a042901dbc
davidm123456 <davidatduke1@gmail.com>
2023-03-17 02:30:25 -0700

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
             <h1 class="font-weight-dark">About</h1>
             <p style={{ fontFamily: "Arial", fontSize: "20px", icon: [<SmileOutlined key="copy-icon" />,
              <SmileFilled key="copied-icon" />], tooltips: ['click here', 'you clicked!!'] }}>
-            This React-run website is apart of a cumulative project created for 2023 <br/ >Winter Quarter CS 35L. 
+            This React based website is apart of a cumulative project created for 2023 <br/ >Winter Quarter CS 35L. 
              This was created by a group of students. Josh Taylor, <br/ >Aryan Janolkar, Nate Carmon,Brian Young and David Marsh. 
             </p>
           <Divider/>
