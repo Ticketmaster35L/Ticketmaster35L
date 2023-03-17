d543324a480c5a8a02217b00fb96481d7c973d3b
davidm123456 <davidatduke1@gmail.com>
2023-03-17 02:25:05 -0700

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,29 @@
 import React from "react";
+import{ SmileFilled, SmileOutlined} from "@ant-design/icons";
+import {Divider} from 'antd';
 
 function About() {
   return (
     <div className="about">
       <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
+        <div class="center my-15 color:#3f6600">
+          <div class="center-lg-10 my-5">
+            <h1 class="font-weight-dark">About</h1>
+            <p style={{ fontFamily: "Arial", fontSize: "20px", icon: [<SmileOutlined key="copy-icon" />,
+             <SmileFilled key="copied-icon" />], tooltips: ['click here', 'you clicked!!'] }}>
+            This React-run website is apart of a cumulative project created for 2023 <br/ >Winter Quarter CS 35L. 
+             This was created by a group of students. Josh Taylor, <br/ >Aryan Janolkar, Nate Carmon,Brian Young and David Marsh. 
+            </p>
+          <Divider/>
+          <img
+              className="img-fluid rounded mb-0 mb-lg-0"
+              src=""
               alt=""
             />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+)}
 
 export default About;
